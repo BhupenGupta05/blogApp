714550059764f3dcbdd918b63c86dc81f9740e42
bhupengupta05@gmail.com <bhupengupta05@gmail.com>
2024-01-14 00:15:33 +0530

    changes

--- src/store.js
+++ src/store.js
@@ -1,20 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './reducers/notificationReducer.js'
 import userReducer from './reducers/userReducer.js'
-<<<<<<< HEAD
 import blogReducer from './reducers/blogReducer.js'
-=======
->>>>>>> 6eaa59d30e8109b6cf49dcb5eee1f8bd5e91f013
 
 const store = configureStore({
     reducer: {
       notification: notificationReducer,
-<<<<<<< HEAD
       user: userReducer,
       blogs: blogReducer
-=======
-      user: userReducer
->>>>>>> 6eaa59d30e8109b6cf49dcb5eee1f8bd5e91f013
     }
 })
 
